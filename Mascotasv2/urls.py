5f3f339882a9756e7a507ba019cf196eb432b201
Shadowpride <60741137+Shadowpride@users.noreply.github.com>
2020-07-08 12:55:40 -0300

    base y extras

--- Mascotasv2/urls.py
+++ Mascotasv2/urls.py
@@ -20,4 +20,6 @@ from Mascotasv2 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Inicio, name='Inicio'),
+    path('Adopciones/', views.Adopciones, name='Adopciones'),
+    path('Busquedas/', views.Busquedas, name='Busquedas'),
 ]
--- Mascotasv2/views.py
+++ Mascotasv2/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def Inicio(request):
     return render(request,"Inicio.html")
+
+def Adopciones(request):
+    return render(request,"Adopciones.html")
+
+def Busquedas(request):
+    return render(request,"Busquedas.html")
